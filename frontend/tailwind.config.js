137d839887283379d8cb7462c24e1bcfc790cbe9
alognz <ag131@illinois.edu>
2024-11-16 19:31:21 -0600

    reinstalled frontend again

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,17 @@
-import {nextui} from '@nextui-org/theme'
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
-}
+  plugins: [],
+};
