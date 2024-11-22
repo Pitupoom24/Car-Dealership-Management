0138586bd3e868b26e3348cfd79ae936191d3dd2
Pitupoom <pitupoomatom139@gmail.com>
2024-11-22 00:56:10 -0600

    implement create/update/delete on backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -14,9 +14,12 @@ class Cars(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     mileage = models.IntegerField(blank=True, null=True)
     status = models.CharField(max_length=50, blank=True, null=True)
-    make = models.ForeignKey('Details', on_delete=models.CASCADE, db_column='make')
-    model = models.ForeignKey('Details', on_delete=models.CASCADE, db_column='model', related_name='cars_model_set')
-    year = models.ForeignKey('Details', on_delete=models.CASCADE, db_column='year', related_name='cars_year_set')
+    make = models.CharField(max_length=50, blank=True, null=True)
+    model = models.CharField(max_length=50, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    # make = models.ForeignKey('Details', on_delete=models.SET_NULL, db_column='make', blank=True, null=True)
+    # model = models.ForeignKey('Details', on_delete=models.SET_NULL, db_column='model', related_name='cars_model_set', blank=True, null=True)
+    # year = models.ForeignKey('Details', on_delete=models.SET_NULL, db_column='year', related_name='cars_year_set', blank=True, null=True)
     locationid = models.ForeignKey('Locations', on_delete=models.CASCADE, db_column='locationID')  # Field name made lowercase.
     lastmodifiedby = models.ForeignKey('Employees', on_delete=models.CASCADE, db_column='lastModifiedBy')  # Field name made lowercase.
     warrantyid = models.ForeignKey('Warranties', on_delete=models.SET_NULL, db_column='warrantyID', blank=True, null=True)  # Field name made lowercase.
@@ -41,6 +44,7 @@ class Cars(models.Model):
 #         db_table = 'Cars'
 
 class Details(models.Model):
+    id = models.AutoField(primary_key=True)  # Explicitly define the primary key
     make = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     year = models.IntegerField()
@@ -51,7 +55,7 @@ class Details(models.Model):
     class Meta:
         managed = False  # Django will not manage the creation or modification of this table
         db_table = 'Details'  # Map to the existing 'Details' table in the database
-        unique_together = ('make', 'model', 'year')  # Enforce uniqueness on these fields
+        # unique_together = ('make', 'model', 'year')  # Enforce uniqueness on these fields
         constraints = [
             models.UniqueConstraint(fields=['make', 'model', 'year'], name='unique_details')
         ]
